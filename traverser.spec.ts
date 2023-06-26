81bcd3e3d9ab0f685628a39fea1858d3d8286641
panther <22311632+lil-nail@users.noreply.github.com>
2023-06-26 15:00:59 +0000

    ✨ feat(traverser): add traverser.ts & traverser.spec.ts

---
+++ traverser.spec.ts
@@ -0,0 +1,51 @@
+import { expect, test } from "vitest"
+import { NodeTypes } from "./parser"
+import {traverser } from "./traverser"
+
+test('traverser ast', () => {
+  const ast = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: NodeTypes.CallExpression,
+        name: 'add',
+        params: [
+          {
+            type: NodeTypes.NumberLiteral,
+            value: '2'
+          },
+          {
+            type: NodeTypes.CallExpression,
+            name: 'subtract',
+            params: [
+              {
+                type: NodeTypes.NumberLiteral,
+                value: '4'
+              },
+              {
+                type: NodeTypes.NumberLiteral,
+                value: '2'
+              }
+            ]
+          },
+        ]
+      },
+    ]
+  }
+
+  // -> Program (enter)
+  //   -> CallExpression (enter)
+  //     -> Number Literal (enter)
+  //     <- Number Literal (exit)
+  //     -> Call Expression (enter)
+  //        -> Number Literal (enter)
+  //        <- Number Literal (exit)
+  //        -> Number Literal (enter)
+  //        <- Number Literal (exit)
+  //     <- CallExpression (exit)
+  //   <- CallExpression (exit)
+  // <- Program (exit)
+  expect(traverser(ast)).toEqual({
+      
+  })
+})
