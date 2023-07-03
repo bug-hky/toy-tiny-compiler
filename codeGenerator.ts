cdaf8656defda35395b11fa78ebdafa9273973c1
panther <22311632+lil-nail@users.noreply.github.com>
2023-07-03 11:04:14 +0000

    ✨ feat(code generator): add code generator function

---
+++ codeGenerator.ts
@@ -0,0 +1,37 @@
+import { RootNode, ChildNode, NodeTypes } from './parser';
+
+/**
+ *  *                  LISP                      C
+ *
+ *   2 + 2          (add 2 2)                 add(2, 2)
+ *   4 - 2          (subtract 4 2)            subtract(4, 2)
+ *   2 + (4 - 2)    (add 2 (subtract 4 2))    add(2, subtract(4, 2))
+*/
+
+export const codeGenerator = (node: any) => {
+  
+  switch (node.type) {
+    case [NodeTypes.Program]:
+      return node.body.map(codeGenerator);
+      break;
+    case [NodeTypes.ExpressionStatement]:
+      return codeGenerator(node.expression);
+      break;
+    case [NodeTypes.CallExpression]:
+      return codeGenerator(node.callee) + '(' + node.arguments.map(codeGenerator).join('') + ')' 
+      break;
+    case [NodeTypes.Identify]:
+      return node.name;
+      break;
+    case [NodeTypes.NumberLiteral]:
+      return node.value
+      break;
+    case [NodeTypes.StringLiteral]:
+      return node.value
+      break;
+    default:
+      break;
+  }
+
+  return node;
+}
