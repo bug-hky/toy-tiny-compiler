7da5f4ba225d599a6ead44ee5ad0038c210645cb
panther <22311632+lil-nail@users.noreply.github.com>
2023-07-03 11:24:25 +0000

    🧪 test(code generator): complete test demo

--- codeGenerator.spec.ts
+++ codeGenerator.spec.ts
@@ -1,6 +1,16 @@
 import { expect, test } from 'vitest'
 import { codeGenerator } from './codeGenerator'
+import { transformerAst } from './transformer.spec'
+
+/**
+ *  *                  LISP                      C
+ *
+ *   2 + 2          (add 2 2)                 add(2, 2)
+ *   4 - 2          (subtract 4 2)            subtract(4, 2)
+ *   2 + (4 - 2)    (add 2 (subtract 4 2))    add(2, subtract(4, 2))
+*/
 
 test('test code generator', () => {
-  
+    const targetCode = 'add(2, subtract(4, 2))';
+    expect(codeGenerator(transformerAst)).toEqual(targetCode)
 })
